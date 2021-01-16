c9d0ffd84275c21746883a6f63a3f9f9ead2e954
xxzhub <1316143408@qq.com>
2021-01-16 19:18:26 +0800

    init

---
+++ src/enums/roleEnum.ts
@@ -0,0 +1,5 @@
+export enum RoleEnum {
+  ADMIN = 'admin',
+
+  TEST = 'test'
+}
---
+++ src/router/types.ts
@@ -0,0 +1,46 @@
+import type { RouteRecordRaw} from 'vue-router'
+import type {Component} from '@/components/types' // 组件类型
+import {RoleEnum} from '@/enums/roleEnum'
+
+// 路由META接口配置
+
+export interface RouteMeta {
+  // 路由title 
+  title: string;
+  //图标
+  icon?: string;
+  //是否忽略权限,只在权限模式为role有效
+  openAuth?: boolean;
+  // 可以访问的角色
+  roles?:RoleEnum[];
+  //是否开启keepalive缓存
+  openKeepAlive?:boolean;
+  //内嵌iframe地址
+  frameSrc?:string;
+  //是否隐藏在面包屑上显示
+  hideBreadcrumb?:boolean;
+  //是否固定标签
+  affix?:boolean;
+  //当前激活菜单
+  currentAvtiveMenu?:boolean;
+  //当前路由不在菜单显示
+  hideMenu?:boolean;
+  //当前路由不在标签页显示
+  hideTab?:boolean;
+}
+
+//路由匹配接口
+export interface AppRouteRecordRaw extends Omit<RouteRecordRaw,'meta'> {
+  //路由名称
+  name:string;
+  //路由信息
+  meta:RouteMeta;
+  //组件
+  component?:Component | string;
+  //组件列表
+  components?:Component;
+  //子组件
+  children?:AppRouteRecordRaw[];
+  //属性参数
+  props?: Recordable;
+}
